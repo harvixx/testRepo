504459b61f306ac1decdc6e3cb023fb1cae6c3ea
Harishlohar0 <harishlohar77777@gmail.com>
2026-04-27 14:58:19 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require("dotenv");
+dotenv.config();
 app.get('/', (req, res) => {
   res.send('hello'); // Yeh browser par dikhega
 });
-
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, () => console.log(`Server is up on port ${PORT}`))
 app.listen(3000, () => console.log('Server is up!'));
