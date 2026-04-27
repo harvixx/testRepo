2c3d61ebd02decea5a7b3ce48dbc3f55b0b089d6
Harishlohar0 <harishlohar77777@gmail.com>
2026-04-27 14:52:03 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello'); // Yeh browser par dikhega
+});
+
+app.listen(3000, () => console.log('Server is up!'));
